313f9a67704ad4a036ac52d869fc0f71f406ce9e
practical <filipvon@protonmail.com>
2022-12-04 19:12:32 +0100

    Day 3 pt 1 - fixed memory error

--- day_3/rucksack.c
+++ day_3/rucksack.c
@@ -6,7 +6,7 @@
 /*   By: practical <install gentoo>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 14:04:15 by practical         #+#    #+#             */
-/*   Updated: 2022/12/04 13:22:15 by practical        ###   ########.fr       */
+/*   Updated: 2022/12/04 19:12:02 by practical        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ int	main(void)
 
 		char *s1 = malloc(len + 1);				// string splitting
 		memcpy(s1, buf, len);					// copies into s1 from buf of length len
-		s1[len + 1] = '\0';						
+		s1[len] = 0;						
 		
 		char *s2 = malloc(len + 1);				// ditto
 		memcpy(s2, (buf + len), len);			// except an offset here, as we want the second half
-		s2[len + 1] = '\0';
+		s2[len] = 0;
 
-		char *same = malloc(len + 1);			// for storing out similar strings
+		char *same = malloc(len);			// for storing out similar strings
 
 		while (s1[i] != '\0')
 		{
@@ -52,17 +52,17 @@ int	main(void)
 			i++;
 		}
 		
-		if (same[0] >= 'a' && same[0] <= 'z')
-			count += same[0] - 96;
-		else 
-			count += same[0] - 37;
-		
-		printf("same:  %s priority %i \n", same, count);
+		if (same[0] > 96 && same[0] < 123)
+			count += (same[0] - 96);
+		else if (same[0] > 64 && same[0] < 91)
+			count += (same[0] - 38);
 
 		free (same);
+		free (s1);
+		free (s2);
 
 	}
-	printf("Your priorities part one:  %d \n", count);
+	printf("Your priorities part one:  %i \n", count);
 	// printf("Your score part two:  %d \n", ptwo);
 	
 	fclose(input);
